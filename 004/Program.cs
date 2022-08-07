e9d4f71e1e16ef7527a9870090115a05a6b1a4f0
Savenkova Mariya <sm3989@yandex.ru>
2022-08-08 01:59:30 +0300

    add 004

---
+++ 004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вводится число N, а затем - N чисел.
+//Определить, сколько среди них пар одинаковых чисел.
+
+using System;
+Console.ReadLine();
+string s=Console.ReadLine();
+string[] ss=s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+int[] a=Array.ConvertAll<string,int>(ss,int.Parse);
+
+int c=0;
+for(int i=0;i<a.Length;i++)
+{
+    for(int j=i+1;j<a.Length;j++)
+    {
+        if(a[i]==a[j]) c++;
+    }
+}
+System.Console.WriteLine(c);
