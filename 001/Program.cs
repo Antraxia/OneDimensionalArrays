1d6ced1b63254e0d23b8d833123330a28feebe4b
Savenkova Mariya <sm3989@yandex.ru>
2022-08-08 00:18:09 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,16 @@
+﻿//Вводится сначала число N, а затем N чисел. Выведите эти N чисел в обратном порядке.
+
+//Входные данные
+//Вводится число N (100>N>0), а затем N чисел из диапазона Integer.
+
+//Выходные данные
+//Выведите N чисел в обратном порядке
+using System;
+
+int N=int.Parse(Console.ReadLine());
+string s=Console.ReadLine();
+string[] ss=s.Split(' ');
+for(int i=ss.Length-1;i>=0;i--)
+{
+    System.Console.Write($"{ss[i]} ");
+}
